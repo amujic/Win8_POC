7b8d2e2d34496a5a8cfc19ea8a954c417dad5d8d
Anthony Mujic <Anthony.Mujic@espn.com>
2012-09-11 21:29:44 -0400

    Initial Commit

---
+++ TestLoginIframe/TestLoginIframe/js/default.js
@@ -0,0 +1,30 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    window.onload = function () {
+
+        window.addEventListener("message", showMyMessage);
+    }
+
+
+    function showMyMessage(e) {
+
+        var output = document.getElementById("output");
+
+        if (e.data == "hide") {
+            document.getElementById("loginiframe").style.visibility = "hidden";
+            output.style.styleFloat = "left";
+
+        }
+        else {
+            document.getElementById("loginiframe").style.visibility = "";
+
+        }
+        output.innerText = e.data;
+
+
+    }
+
+})();
---
+++ TestLoginIframe/TestLoginIframe/js/pagetest.js
@@ -0,0 +1,14 @@
+﻿window.onload = function () {
+
+    document.getElementById("submit").addEventListener("click", submitForm, false);
+    
+
+}
+
+function submitForm(e) {
+
+    var message = document.getElementById("msg").value;
+    window.parent.postMessage(message, "/")
+
+
+}
